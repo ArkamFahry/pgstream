faa66ff364769c0d708d0959d5b546c51934990a
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-03-09 12:23:18 +0530

    chore: updated the base signal model to have a data blob

--- models/signal_model.go
+++ models/signal_model.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 const (
 	SignalTable = "pgwarp.signals"
 )
@@ -14,18 +12,7 @@ const (
 )
 
 type Signal struct {
-	Id        string    `json:"id" db:"id"`
-	Type      string    `json:"type" db:"type"`
-	Database  string    `json:"database" db:"database"`
-	Schema    string    `json:"schema" db:"schema"`
-	Tables    []string  `json:"tables" db:"tables"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-}
-
-type SnapshotPause struct {
-	Id        string `json:"id" db:"id"`
-	SignalId  string `json:"signal_id" db:"signal_id"`
-	Point     string `json:"pause_point" db:"point"`
-	ResumedAt string `json:"resumed_at" db:"resumed_at"`
-	CreatedAt string `json:"created_at" db:"created_at"`
+	Id   string         `json:"id" db:"id"`
+	Type string         `json:"type" db:"type"`
+	Data map[string]any `json:"data" db:"data"`
 }
